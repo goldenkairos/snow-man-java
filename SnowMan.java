8fd4d08728ed82bd47f2734f00a4889f85793225
Minh <minh.seikel@gmail.com>
2023-11-22 16:59:21 -0800

    adding a method to pull random words from words.txt and another method to select a random word that meets the min and max length requirements

--- SnowMan.java
+++ SnowMan.java
@@ -1,7 +1,17 @@
+
+// import org.apache.commons.lang3.RandomStringUtils;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Collector;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+//https://replit.com/@ViktoriiaZoloto/viktoriiasnowmanprojectC18#game.py
 
 public class SnowMan {
     private static final int SNOWMAN_MIN_WORD_LENGTH = 5;
@@ -15,6 +25,13 @@ public class SnowMan {
     private static final String SNOWMAN_5 = "*  \\( : )/ *";
     private static final String SNOWMAN_6 = "  *(_ : _)  *";
     private static final String SNOWMAN_7 = "-----------";
+
+    private static final String WORDS_FILE_PATH = "words.txt";
+    private static List<String> WORDS;
+
+    static {
+        WORDS = loadWordsFromFile();
+    }
 
     public static void main(String args[]) {
 
@@ -32,11 +49,52 @@ public class SnowMan {
         boolean res = getWordProgress("HelloWord", wordMap);
         System.out.println(res);
 
-        ArrayList<Character> wrongGuessList = new ArrayList<>(Arrays.asList('b','c','a'));
+        ArrayList<Character> wrongGuessList = new ArrayList<>(Arrays.asList('b', 'c', 'a'));
 
-        String uInput = getLetterfromUser(wordMap, wrongGuessList);
+        // String uInput = getLetterfromUser(wordMap, wrongGuessList);
 
+        // String Word =
+        // snowmanWordGenerator(SNOWMAN_MIN_WORD_LENGTH,SNOWMAN_MAX_WORD_LENGTH);
 
+        // System.out.println(Word);
+
+        String secretWord = getRandomWord(SNOWMAN_MIN_WORD_LENGTH, SNOWMAN_MAX_WORD_LENGTH);
+        System.out.println("Secret Word: " + secretWord);
+
+    }
+
+    // This method is designed to load words from words.txt
+    private static List<String> loadWordsFromFile() {
+        List<String> words = new ArrayList<>();
+
+        // starts a try-with-resources block. It creates a BufferedReader to efficiently
+        // read the contents of a file specified by the WORDS_FILE_PATH.
+        // The try-with-resources ensures that the BufferedReader is closed properly
+        // after use.
+        try (BufferedReader reader = new BufferedReader(new FileReader(WORDS_FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                words.add(line.trim());
+            }
+
+            // This block catches any IOException that might occur during the file reading
+            // process.
+            // If an exception occurs, it prints the stack trace to the console using
+            // e.printStackTrace().
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return words;
+    }
+
+    public static String getRandomWord(int minLen, int maxLen) {
+
+        List<String> filteredWordList = WORDS.stream()
+                .filter(word -> word.length() >= minLen && word.length() <= maxLen).collect(Collectors.toList());
+
+        Random random = new Random();
+        int randomIndex = random.nextInt(filteredWordList.size());
+        return filteredWordList.get(randomIndex);
     }
 
     // This function prints out the appropriate snowman image
@@ -80,7 +138,7 @@ public class SnowMan {
     // It prints an output_string that shows the correct letter guess placements
     // as well as the placements for the letters yet to be guessed.
     public static void printWordProgressString(String snowmanWord, HashMap<Character, Boolean> snowmanHashMap) {
-        // String progressString = "";
+        // String progressString = ""; //string is immutable
         StringBuilder progressString = new StringBuilder();
 
         for (char letter : snowmanWord.toCharArray()) {
@@ -110,41 +168,43 @@ public class SnowMan {
     // that have been guessed incorrectly (wrong_guesses_list) as input.
     // It asks for input from the user of a single character until
     // a valid character is provided and then returns this character.
-    public static String getLetterfromUser(HashMap<Character, Boolean> snowmanHashMap, ArrayList<Character> wrongGuessArr){
+    public static String getLetterfromUser(HashMap<Character, Boolean> snowmanHashMap,
+            ArrayList<Character> wrongGuessArr) {
 
         boolean validInput = false;
         Scanner scanner = new Scanner(System.in);
-        String userInputString ="";
+        String userInputString = "";
 
         while (!validInput) {
             System.out.println("Guess a letter: ");
             userInputString = scanner.nextLine();
 
-            if (userInputString.length() > 1){
+            if (userInputString.length() > 1) {
                 System.out.println("You must only enter 1 letter at a time!");
-            } else if (userInputString.length() == 1){
-            char userInputChar = userInputString.charAt(0);
-                if (!Character.isLetter(userInputChar)){
-                System.out.println("You must input a letter!");
-            } else if (snowmanHashMap.containsKey(userInputChar) && snowmanHashMap.get(userInputChar)){
-                System.out.println("You already guesses that letter and it's in the word!");
-            } else if (isCharinArr(userInputChar, wrongGuessArr)){
-                System.out.println("You already guessed that letter and it's not in the word!");
-            } else {
-                validInput = true;
+            } else if (userInputString.length() == 1) {
+                char userInputChar = Character.toLowerCase(userInputString.charAt(0));
+                if (!Character.isLetter(userInputChar)) {
+                    System.out.println("You must input a letter!");
+                } else if (snowmanHashMap.containsKey(userInputChar) && snowmanHashMap.get(userInputChar)) {
+                    System.out.println("You already guessed that letter and it's in the word!");
+                } else if (isCharinArr(userInputChar, wrongGuessArr)) {
+                    System.out.println("You already guessed that letter and it's not in the word!");
+                } else {
+                    validInput = true;
+                }
             }
         }
-    }
         scanner.close();
         return userInputString;
-}
+    }
 
     public static boolean isCharinArr(char c, ArrayList<Character> arr) {
         for (char ch : arr) {
-            if (c == ch) {
+            if (Character.toLowerCase(c) == Character.toLowerCase(ch)) {
                 return true;
             }
         }
         return false;
     }
+
 }
